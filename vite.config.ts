d211bf9678f0d61662130917b3b0733c682f6e46
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2024-04-06 22:34:29 +0530

    fix build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   resolve: {
     alias: {
       "@": "/src",
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        index1: resolve(__dirname, "index1.html"),
+        index2: resolve(__dirname, "index2.html"),
+      },
     },
   },
 });
